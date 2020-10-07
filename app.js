b1f417e1d23b791fe98721962a28a8293e562f94
EmJee <mart-janroeleveld@outlook.com>
2020-10-07 13:44:01 +0200

    Small update to fix conflicting port numbers

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 const router = require('./router/router')
 
 // get the port number from the .env, if it returns undefined set it to 3000
-const port = process.env.PORT | 3000
+const port = process.env.PORT
 
 // use global middleware
 app.use(bodyParser.json())
